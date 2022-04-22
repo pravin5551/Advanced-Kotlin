30f8ba325e49e7367d031cf411872a285084b84a
Pravin Pawar <pravinvpawar5551@gmail.com>
2022-04-22 10:03:39 +0530

    Infix Example modified

--- app/src/main/java/com/example/advancekotlin/Infix.kt
+++ app/src/main/java/com/example/advancekotlin/Infix.kt
@@ -1,25 +1,25 @@
 package com.example.advancekotlin
 
-class Infix{
+class Infix {
 
-/**
- * Infix notation in Kotlin
+    /**
+     * Infix notation in Kotlin
 
-Functions marked with the infix keyword can also be called using the infix notation (omitting the dot and the parentheses for the call).
+    Functions marked with the infix keyword can also be called using the infix notation (omitting the dot and the parentheses for the call).
 
-Infix functions must satisfy the following requirements:
+    Infix functions must satisfy the following requirements:
 
-- They must be member functions or extension functions;
-- They must have a single parameter;
-- The parameter must not accept the variable number of arguments and must have no default value.
- */
+    - They must be member functions or extension functions;
+    - They must have a single parameter;
+    - The parameter must not accept the variable number of arguments and must have no default value.
+     */
 
 
-   // https://www.facebook.com/mindorks.nextgen/posts/infix-notation-in-kotlinfunctions-marked-with-the-infix-keyword-can-also-be-call/1906137586191012/
+    // https://www.facebook.com/mindorks.nextgen/posts/infix-notation-in-kotlinfunctions-marked-with-the-infix-keyword-can-also-be-call/1906137586191012/
 }
 
-fun main(){
-    infix fun Int.add(value:Int):Int = this + value
+fun main() {
+    infix fun Int.add(value: Int): Int = this + value
 
     //without infix
     val sum = 5.add(10)
