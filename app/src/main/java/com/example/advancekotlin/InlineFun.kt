50ea4a08834e93279caba55562a6689eec981ec5
Pravin Pawar <pravinvpawar5551@gmail.com>
2022-04-23 11:57:38 +0530

    Inline advance fun example

---
+++ app/src/main/java/com/example/advancekotlin/InlineFun.kt
@@ -0,0 +1,28 @@
+package com.example.advancekotlin
+
+/**
+ * Higher Order Functions and Inline.
+ */
+class InlineFun {
+}
+
+fun main() {
+    /*
+    An inline fun is declared with a keyword inline.
+    The use of inline fun enhances the performance of higher order function.
+    The inline fun tells the compiler to copy parameters and fun to call site
+     */
+    inlineFunction { print("Inside main") }
+
+    inlineFunction { print("Inside main") }
+
+    inlineFunction { print("Inside main") }
+/*
+It creates single instances of with inline , without inline it creates many instances of same variable
+ */
+}
+
+inline fun inlineFunction(fn: () -> Unit) {
+    fn()
+    print("Code inside Inline Function")
+}
