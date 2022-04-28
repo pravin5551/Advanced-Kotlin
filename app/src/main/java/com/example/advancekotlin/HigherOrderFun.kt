58d53c7c89eee37a67149e9e9058288b00465d84
pravin5551 <pravinvpawar5551@gmail.com>
2022-04-28 10:00:37 +0530

    Higher order fun

---
+++ app/src/main/java/com/example/advancekotlin/HigherOrderFun.kt
@@ -0,0 +1,18 @@
+package com.example.advancekotlin
+
+class HigherOrderFun {
+
+
+}
+
+fun main() {
+    higherOrder(5.0 , 6.0,::add)
+}
+
+fun add(a: Double, b: Double): Double {
+    return a + b;
+}
+
+fun higherOrder(a: Double, b: Double, fn: (Double, Double) -> Double) {
+    print(fn(a, b))
+}
